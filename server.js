9ddafe3faffca6c53121a7b072a83408713aa587
Martin-John Hearty <martinjohn.hearty@gmail.com>
2020-04-13 18:08:41 -0400

    testing lame

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const upload = multer();
 const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
-const Lame = require("lame");
+const lame = require("lame");
 
 // serve static files before gatsbyExpress
 app.use(express.static('public/'));
@@ -123,7 +123,7 @@ app.post('/api/upload', upload.single('soundBlob'), function (req, res, next) {
     let newFileName = req.file.originalname.substring(0, req.file.originalname.length - 4) + '.mp3'
 
     try {
-      const encoder = new Lame({
+      const encoder = new lame.Encoder({
         output:  __dirname + '/public/uploads/' + newFileName,
         bitrate: 192,
       }).setBuffer(Buffer.from(new Uint8Array(req.file.buffer)));
